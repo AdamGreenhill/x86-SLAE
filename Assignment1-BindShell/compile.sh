dc78443f800594e89d10375ebdadb5f48f708617
Sentient <adam.greenhill@gmail.com>
2019-11-16 13:30:03 -0500

    Update compile.sh

--- Assignment1-BindShell/compile.sh
+++ Assignment1-BindShell/compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-nasm -f elf32 bind.nasm
-ld -m elf_i386 bind.o -o bind
-objdump -d ./bind |grep '[0-9a-f]:'|grep -v 'file'|cut -f2 -d:|cut -f1-6 -d' '|tr -s ' '|tr '\t' ' '|sed 's/ $//g'|sed 's/ /\\\x/g'|paste -d '' -s |sed 's/^/"/'|sed 's/$/"/g'
+nasm -f elf32 linux_x86_bind_shell.nasm
+ld -m elf_i386 linux_x86_bind_shell.o -o linux_x86_bind_shell
+objdump -d ./linux_x86_bind_shell |grep '[0-9a-f]:'|grep -v 'file'|cut -f2 -d:|cut -f1-6 -d' '|tr -s ' '|tr '\t' ' '|sed 's/ $//g'|sed 's/ /\\\x/g'|paste -d '' -s |sed 's/^/"/'|sed 's/$/"/g'
